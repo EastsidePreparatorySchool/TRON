38e094b91a9afc640bd78a513d139c928f47b67e
Peter Kavounas <PKavounas@eastsideprep.org>
2019-05-20 09:27:04 -0700

    add intarray for position

---
+++ src/main/java/org/eastsideprep/tron/bikeContainer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eastsideprep.tron;
+
+/**
+ *
+ * @author pkavounas
+ */
+public class bikeContainer { 
+    bike bike;
+    //intarray PUT HERE 
+           
+}
