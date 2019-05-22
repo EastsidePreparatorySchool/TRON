d411029a619f2d5d39e9a6a50fb9021e5c5808fd
Audrey Whitmer <Audrey@Whitmer.local>
2019-05-22 14:28:39 -0700

    added grid helper and orbit controls

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,5 +1,31 @@
 //client side of TRON
 
+const WIDTH = 1500; //innerWidth; //change these back!!
+const HEIGHT = 700; //innerHeight;
+
+const VIEW_ANGLE = 45;
+const ASPECT = WIDTH / HEIGHT;
+const NEAR = 0.1;
+const FAR = 10000;
+
+const container = document.querySelector('#container');
+
+const renderer = new THREE.WebGLRenderer();
+var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+var controls = new THREE.OrbitControls(camera);
+const scene = new THREE.Scene();
+
+camera.position.set(0, 10, 10);
+var cameraFocus = new THREE.Vector3(0, 0, 0);
+camera.lookAt(cameraFocus);
+controls.update();
+
+scene.add(camera);
+renderer.setSize(WIDTH, HEIGHT);
+container.appendChild(renderer.domElement);
+
+
+//now the game starts
 bikes = []; //store IDs of each bike in the game
 
 class Bike {
@@ -18,3 +44,36 @@ class Bike {
         delete bikes[this.id];
     }
 }
+
+
+var gridSize = 10;
+var gridDivisions = 100;
+var gridCenterColor = 0x66ffd9; //0x444444;
+var gridColor = 0x668cff; //0x888888;
+
+//grid helper constructor: (size, divisions, colorCenterLine, colorGrid)
+var grid = new THREE.GridHelper (gridSize, gridDivisions, gridCenterColor, gridColor);
+scene.add (grid);
+
+
+
+
+
+
+//
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+scene.add(pointLight);
+
+renderer.render(scene, camera);
+
+function update () {
+    renderer.render(scene, camera);
+    controls.update();
+    requestAnimationFrame(update); //Schedule the next frame.
+}
+
+requestAnimationFrame(update); // Schedule the first frame.
