13aca60f220172b0621c09d33d645984a28f7bec
TEspelien <tespelien@eastsideprep.org>
2019-06-11 14:30:23 -0700

    intermidiate fix: more to come

--- src/main/java/org/eastsideprep/tron/main.java
+++ src/main/java/org/eastsideprep/tron/main.java
@@ -25,7 +25,7 @@ import static spark.Spark.*;
 public class main {
 
     public final static int LENGTH = 100;
-    public final static int BIKES = 2;
+    public final static int BIKES = 4;
     public final static TronGameState STATE = new TronGameState(true);
     static Connection conn = null;
 
@@ -34,33 +34,33 @@ public class main {
     static AbstractGameEngine PreSetGame = new AbstractGameEngine(0, "engineTest", 250, PreSetBikes);
 
     public static void main(String[] args) {
-          char quote = '"';
+        char quote = '"';
         staticFiles.location("public/");
 
         before("*", (req, res) -> {
             //System.out.println("request coming in: " + req.requestMethod() + ":" + req.url());
         });
         //We don't need SQL for testing!
-        connect();
+        //connect();
 
         //full functionality
         //updateBikeTest();
-        get("/updateBikes", "application/json", (req, res) -> updateBikes(req), new JSONRT());
-        post("/createGame", "application/json", (req, res) -> newGame(req));
-        get("/getGames", "application/json", (req, res) -> getGamesTable(req), new JSONRT());
-        get("/initializeBikes", "application/json", (req, res) -> initializeBikes(), new JSONRT());
-        get("/runGame", "application/json", (req, res) -> runGame(req), new JSONRT());
-
+        //get("/updateBikes", "application/json", (req, res) -> updateBikes(req), new JSONRT());
+        //post("/createGame", "application/json", (req, res) -> newGame(req));
+        //get("/getGames", "application/json", (req, res) -> getGamesTable(req), new JSONRT());
+        //get("/initializeBikes", "application/json", (req, res) -> initializeBikes(), new JSONRT());
+        //get("/runGame", "application/json", (req, res) -> runGame(req), new JSONRT());
         //for testing purposes only
-        get("/updateBikeTest", "application/json", (req, res) -> updateBikeTest(), new JSONRT());
-        post("/runGameTest", "application/json", (req, res) -> runGameTest(req), new JSONRT());
-        giveMeTheValue("GameID","games","GameName= " +quote + "Gametest"+ quote);
-        
+        get("/updateBikes", "application/json", (req, res) -> updateBikes(), new JSONRT());
+        get("/getBoard", "application/json", (req, res) -> getBoard(), new JSONRT());
+        post("/runGame", "application/json", (req, res) -> runGame(req), new JSONRT());
+        //giveMeTheValue("GameID", "games", "GameName= " + quote + "Gametest" + quote);
+
     }
 
-//TEST
+//Trajan's methods (work better...)
     //testing methods
-    private static Object[] runGameTest(Request req) {
+    private static Object[] runGame(Request req) {
         MultipartConfigElement multipartConfigElement = new MultipartConfigElement(System.getProperty("java.io.tmpdir"));
         req.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
         String type = req.queryParams("type");
@@ -89,136 +89,13 @@ public class main {
         }
         return new String[]{"oof"};
     }
-   
-    
 
-//list of bikeos and such
-    private static Object[] initializeBikes() {
-        try {
-            ArrayList<GameLogEntry> log = STATE.getCompactedEntries();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return null;
+    private static Object[] updateBikes() {
+        return
     }
 
-    private static ServerContext getCtx(spark.Request req) {
-        HashMap<String, ServerContext> ctxMap = req.session().attribute("ServerContexts");
-        if (req.session().isNew() || ctxMap == null) {
-            ctxMap = new HashMap<>();
-            req.session().attribute("ServerContexts", ctxMap);
-        }
-
-        String client = req.queryParams("clientID");
-        ServerContext ctx = ctxMap.get(client);
-        if (ctx == null) {
-            ctx = new ServerContext();
-            ctx.clientSubID = client;
-            ctxMap.put(client, ctx);
-        }
-//I COMENTED THIS ONE OUT BECAUSE IT WAS GIVING AN ERROR AND IT WAS ANNOYING
-        // blow up stale contexts
-
-        // if (ctx.observer != null && ctx.observer.isStale()) { 
-        //     ctx.observer = null;
-//            return null;
-        //  }
-        //if (ctx.observer != null && ctx.observer.isStaxle()) {
-        //ctx.observer = null;
-//            return null;
-        //}
-        req.session().maxInactiveInterval(60); // kill this session afte 60 seconds of inactivity
-        return ctx;
-    }
-
-//smol update stuff
-//send bike position, added trail position of bike
-    private static Object[] updateBikes(spark.Request req) {
-        try {
-            ServerContext ctx = getCtx(req);
-            if (ctx.observer == null) {
-                return null;
-            }
-
-            ArrayList<GameLogEntry> list = ctx.observer.getNewItems();
-
-            if (req.queryParams("compact").equals("yes")) {
-                // if the client requests it, make a new game state, 
-                // we will use it to compact the entries
-
-                TronGameState compactor = new TronGameState(true);
-                for (GameLogEntry item : list) {
-                    compactor.addEntry(item);
-                }
-                list = compactor.getCompactedEntries();
-            }
-            // now convert into array to JSON knows what to do
-            TronGameLogEntry[] array = new TronGameLogEntry[list.size()];
-            return list.toArray(array);
-
-        } catch (Exception e) {
-            System.out.println("exception in GU");
-            System.out.println(e.getMessage());
-            e.printStackTrace(System.out);
-        }
-        //return null;
-
-        try {
-            ArrayList<GameLogEntry> log = STATE.getCompactedEntries();
-            Object[] result = new Object[4];
-            //result be like [bike#, bikes, trails, deaths]
-            result[0] = BIKES;
-
-            ArrayList<Object> tempList = new ArrayList<>();
-            ArrayList<Object> bikeInfo = new ArrayList<>();
-            //info be like {bikeID, (int[] bikePos) [x,y]}
-            ArrayList<Object> trailInfo = new ArrayList<>();
-            //info be like {bikeid, (int[] trailPos) [x,y]}
-            ArrayList<Object> deathInfo = new ArrayList<>();
-            //info be like {bikeID} (means bike with that bikeID died)
-            int nullID = BIKES + 1;
-// I SWITCHED "bikes" to "BIKES" because it was giving an error and it was annoying. Sorry if that wasnt supposed to happen- theo 
-            for (int i = 1; i < BIKES + 1; i++) {
-                bikeInfo.add(log.get(i));
-                //bikeInfo.add(log.get(i).id); //adding id first
-                //bikeInfo.add(new int[] {log.get(i).p.x, log.get(i).p.y}); //adding position second
-                tempList.add(bikeInfo);
-                bikeInfo.clear();
-            }
-            result[1] = tempList;
-            tempList.clear();
-
-            for (int i = BIKES + 1; i < log.size(); i++) {
-                while (log.get(i) != null) {
-                    trailInfo.add(log.get(i));
-                    //trailInfo.add(log.get(i).id); //adding id first
-                    //trailInfo.add(new int[] {log.get(i).p.x, log.get(i).p.y}); //adding position second
-                    tempList.add(trailInfo);
-                    trailInfo.clear();
-                }
-                nullID = i;
-                break;
-            }
-            result[2] = tempList;
-            tempList.clear();
-
-            for (int i = nullID; i < log.size(); i++) {
-                deathInfo.add(log.get(i));
-                //deathInfo.add(log.get(i).id);
-            }
-            result[3] = tempList;
-            tempList.clear();
-
-            return result;
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return null;
-    }
-
-    //TODO: get bikes from request and use the .run() method in AbstractGameEngine.java
-    private static Object[] runGame(spark.Request req) {
-        return null;
+    private static Object[] getBoard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
